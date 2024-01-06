806e5752ab6dca11a51f40cd48247f0e9b676e3c
raulmahya <raulmahya11@gmail.com>
2024-01-06 11:37:48 +0700

    push

--- mongo.go
+++ mongo.go
@@ -10,13 +10,6 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func MongoConnect(mconn DBInfo) (db *mongo.Database) {
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(mconn.DBString))
-	if err != nil {
-		fmt.Printf("AIteung Mongo, MongoConnect: %v\n", err)
-	}
-	return client.Database(mconn.DBName)
-}
 func InsertOneDoc(db *mongo.Database, collection string, doc interface{}) (insertedID interface{}) {
 	insertResult, err := db.Collection(collection).InsertOne(context.TODO(), doc)
 	if err != nil {
